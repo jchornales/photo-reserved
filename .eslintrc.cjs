6fec36af39ca973c5fa07613074cbcad033b95aa
Justine Hornales <jchornales.dev@gmail.com>
2023-01-17 07:38:25 +0800

    f/PR0001: Initialize Project

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+  },
+};
