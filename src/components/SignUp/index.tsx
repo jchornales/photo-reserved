b2bece837d8b1fd50764d729020b1fe90becf828
Justine Hornales <jchornales.dev@gmail.com>
2023-01-18 07:32:19 +0800

    f/PR0003: Initialize Auth & Sign Up

---
+++ src/components/SignUp/index.tsx
@@ -0,0 +1,61 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable no-console */
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { userSchema } from '../../config/Validations/initialize';
+import { FormData } from '../../config/Types/initialize';
+import createUser from '../../config/Firebase/createUser';
+
+export default function SignUp() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(userSchema),
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    if (userSchema.isValidSync(data)) {
+      createUser(data);
+    }
+  });
+
+  // function handleOnSubmit(event: FormEvent) {
+  //   event.preventDefault();
+  // }
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="first_name">First Name</label>
+      <input {...register('first_name')} />
+      <p>{errors.first_name?.message}</p>
+
+      <label htmlFor="last_name">Last Name</label>
+      <input {...register('last_name')} />
+      <p>{errors.last_name?.message}</p>
+
+      <label htmlFor="address">address</label>
+      <input {...register('address')} />
+      <p>{errors.address?.message}</p>
+
+      <label htmlFor="phone">phone</label>
+      <input {...register('phone')} />
+      <p>{errors.phone?.message}</p>
+
+      <label htmlFor="email">Email</label>
+      <input {...register('email')} />
+      <p>{errors.email?.message}</p>
+
+      <label htmlFor="password">Password</label>
+      <input type="password" {...register('password')} />
+      <p>{errors.password?.message}</p>
+
+      <label htmlFor="password">Confirm Password</label>
+      <input type="password" {...register('passwordConfirmation')} />
+      <p>{errors.passwordConfirmation?.message}</p>
+
+      <button type="submit">SetValue</button>
+    </form>
+  );
+}
---
+++ src/config/Firebase/createUser.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { addDoc, collection } from 'firebase/firestore';
+import { auth } from './initialize';
+import { database } from './fetchData';
+import { FormData } from '../Types/initialize';
+
+export default function createUser(data: FormData) {
+  createUserWithEmailAndPassword(auth, data.email, data.password)
+    .then(async (userCredential) => {
+      const { user } = userCredential;
+      console.log(user);
+      try {
+        const docRef = await addDoc(collection(database, 'customers'), {
+          user_uid: user.uid,
+          first_name: data.first_name,
+          last_name: data.last_name,
+          phone: data.phone,
+          address: data.address,
+        });
+        console.log('Document written with ID:', docRef.id);
+      } catch (error) {
+        console.error('Error adding document: ', error);
+      }
+    })
+    .catch((error) => {
+      const errorCode = error?.code;
+      const errorMessage = error?.message;
+      console.log(`${errorCode} : ${errorMessage}`);
+    });
+}
