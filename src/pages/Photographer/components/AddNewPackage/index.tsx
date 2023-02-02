4989694ba60000e7354ec7d15c58c12822bb80de
Justine Hornales <jchornales.dev@gmail.com>
2023-02-02 20:17:19 +0800

    Added inclusion fields

--- src/pages/Photographer/components/AddNewPackage/index.tsx
+++ src/pages/Photographer/components/AddNewPackage/index.tsx
@@ -1,17 +1,21 @@
 import {
+  ActionIcon,
   Group,
+  List,
   NumberInput,
   Paper,
   Select,
   Stack,
   TextInput,
+  ThemeIcon,
+  useMantineTheme,
 } from '@mantine/core';
 import { useForm, Controller } from 'react-hook-form';
 import { AddPackageData } from '../../../../config/Types/PhotographerForm';
 import { UseFormReturn } from 'react-hook-form';
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPesoSign } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faPesoSign, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 type Props = {
   form: UseFormReturn<AddPackageData>;
@@ -88,7 +92,62 @@ function DynamicSelectField({ form }: Props) {
   );
 }
 
-function InclusionField() {}
+type InclusionProps = {
+  id: number | undefined;
+  value: string;
+};
+
+function InclusionField() {
+  const [input, setInput] = useState('');
+  const [inclusions, setInclusions] = useState<InclusionProps[]>([]);
+  const theme = useMantineTheme();
+
+  const handleClick = () => {
+    setInclusions((prevState) => {
+      return [...prevState, { id: inclusions?.length, value: input }];
+    });
+    console.log(inclusions);
+  };
+
+  return (
+    <Stack>
+      <TextInput
+        withAsterisk
+        label="Inclusions"
+        size="md"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        rightSection={
+          <ActionIcon
+            size={32}
+            radius="xl"
+            color={theme.primaryColor}
+            variant="filled"
+            onClick={() => handleClick()}
+          >
+            <FontAwesomeIcon icon={faPlus} />
+          </ActionIcon>
+        }
+        placeholder="Add inclusion services for package"
+        rightSectionWidth={42}
+      />
+      <List
+        spacing="xs"
+        size="sm"
+        center
+        icon={
+          <ThemeIcon color="teal" size={24} radius="xl">
+            <FontAwesomeIcon icon={faCheck} />
+          </ThemeIcon>
+        }
+      >
+        {inclusions?.map((item) => {
+          return <List.Item key={item.id}>{item.value}</List.Item>;
+        })}
+      </List>
+    </Stack>
+  );
+}
 
 export default function AddNewPackage() {
   const form = useForm<AddPackageData>();
@@ -98,6 +157,7 @@ export default function AddNewPackage() {
         <Stack>
           <StaticInputFields form={form} />
           <DynamicSelectField form={form} />
+          <InclusionField />
         </Stack>
       </form>
     </Paper>
